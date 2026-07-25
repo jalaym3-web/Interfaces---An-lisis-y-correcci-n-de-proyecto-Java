b74acb74f56bef9b0a2e0f07c24250277ccbf70d
Jose Alay <jalaym3@miumg.edu.gt>
2026-07-24 20:00:41 -0600

    subir tarea de interfaces

---
+++ src/edu/umg/programacion2/clase01/ejercicios/multimedia/Video.java
@@ -0,0 +1,29 @@
+package edu.umg.programacion2.clase01.ejercicios.multimedia;
+
+/**
+ * TODO (estudiante): completar reproducir() y detener().
+ * <p>
+ * Pista para reproducir(): imprime algo como "Reproduciendo video: <titulo> en
+ * <resolucion>". Pista para detener(): imprime algo como "Video detenido:
+ * <titulo>".
+ */
+public class Video implements Reproducible {
+
+	private final String titulo;
+	private final String resolucion;
+
+	public Video(String titulo, String resolucion) {
+		this.titulo = titulo;
+		this.resolucion = resolucion;
+	}
+
+	@Override
+	public void reproducir() {
+		System.out.println("Reproduciendo video: " + titulo + " Resolución: " + resolucion);
+	}
+
+	@Override
+	public void detener() {
+		System.out.println("Video detenido: " + titulo);
+	}
+}
